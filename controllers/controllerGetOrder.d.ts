e9245ef8bbd739d3a7700bbf1ac9c177feaf39af
Siziy Ivan <cziy@ya.ru>
2020-08-27 18:14:51 +0300

    rebuild with comments

--- controllers/controllerGetOrder.d.ts
+++ controllers/controllerGetOrder.d.ts
@@ -1,1 +1,35 @@
+/**
+ * @api {POST} /api/0.5/order/:number Проверка заказа
+ * @apiName GetOrder
+ * @apiGroup Order
+ * @apiDescription Получение информации о заказе,
+ *
+ * @apiParam {String} number rmsOrderNumber корзины который выдала РМС система заказу пр регстрации
+ *
+ * @apiSuccess {[Cart](#api-Models-ApiCart)} cart Коризна с заполненым поле delivery. Если delivery 0, то доставка бесплатная
+ * @apiSuccess {JSON} message Сообщение
+ *
+ * @apiSuccessExample {JSON} Message:
+ *  {
+      type: 'info',
+      title: 'ok',
+      body: 'success'
+ *  }
+ *
+ * @apiError {[Cart](#api-Models-ApiCart)} cart Прежняя запрашиваемая корзина
+ * @apiError {JSON} message Сообщение об ошибке
+ * @apiError (500) {JSON} ServerError Ошибка сервера
+ *
+ * @apiErrorExample ServerError 500
+ *  {
+ *    message: {
+        type: 'error',
+        title: 'RMS problem',
+        body: data.problem
+      },
+      cart: {
+        ...
+      }
+ *  }
+ */
 export default function (req: ReqType, res: ResType): Promise<any>;
--- controllers/controllerMenu.d.ts
+++ controllers/controllerMenu.d.ts
@@ -1,1 +1,7 @@
+/**
+ * @apiDefine MenuError
+ *
+ * @apiError NotFound 404
+ * @apiError (500) ServerError 500
+ */
 export default function (req: ReqType, res: ResType): Promise<any>;
